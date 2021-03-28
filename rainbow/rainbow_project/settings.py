12988002f5bc2d2c691114a8eef95dc7eab160ed
karina <karina.klinkeviciute@gmail.com>
2021-03-28 19:14:05 +0300

    flatpages middleware

--- rainbow/rainbow_project/settings.py
+++ rainbow/rainbow_project/settings.py
@@ -59,6 +59,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware'
 ]
 
 ROOT_URLCONF = 'rainbow_project.urls'
