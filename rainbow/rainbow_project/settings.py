f27a572fef40ad7a0dbed8718553ebbeb9d5a657
karina <karina.klinkeviciute@gmail.com>
2021-03-28 14:20:42 +0300

    static root setting temporary changed to hardcoded for testing

--- rainbow/rainbow_project/settings.py
+++ rainbow/rainbow_project/settings.py
@@ -140,7 +140,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = '/root/rainbow-challenge/rainbow/staticfiles/'
 
 AUTH_USER_MODEL = 'user.User'
 
