26769bcfb8c7668e8a0c0c102e6ad6085ebc09cd
karina <karina.klinkeviciute@gmail.com>
2021-06-10 14:06:23 +0300

    changed to ssl again

--- rainbow/rainbow_project/settings.py
+++ rainbow/rainbow_project/settings.py
@@ -186,8 +186,8 @@ OTP_TOTP_ISSUER = 'Rainbow challenge'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com/'
-EMAIL_PORT = 587
+EMAIL_PORT = 465
 EMAIL_HOST_USER = 'test@lygiai.org'
 EMAIL_HOST_PASSWORD = 'testtest'
-EMAIL_USE_TLS = True
+EMAIL_USE_SSL = True
 
