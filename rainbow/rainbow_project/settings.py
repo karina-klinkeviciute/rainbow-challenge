3910d8fa0cbf3381f7cb14f90d4710aa186c8750
karina <karina.klinkeviciute@gmail.com>
2021-06-10 13:50:54 +0300

    added email backend

--- rainbow/rainbow_project/settings.py
+++ rainbow/rainbow_project/settings.py
@@ -184,6 +184,7 @@ DJOSER = {
 
 OTP_TOTP_ISSUER = 'Rainbow challenge'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com/'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'test@lygiai.org'
