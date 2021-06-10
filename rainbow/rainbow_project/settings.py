095f2ca83bad0ef788b0e5f3f5c6fca8d072c77a
karina <karina.klinkeviciute@gmail.com>
2021-06-10 17:01:52 +0300

    email not through gmail

--- rainbow/rainbow_project/settings.py
+++ rainbow/rainbow_project/settings.py
@@ -185,9 +185,9 @@ DJOSER = {
 OTP_TOTP_ISSUER = 'Rainbow challenge'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com/'
-EMAIL_PORT = 465
-EMAIL_HOST_USER = 'test@lygiai.org'
-EMAIL_HOST_PASSWORD = 'bandymasbANdymas'
-EMAIL_USE_SSL = True
+# EMAIL_HOST = 'smtp.gmail.com/'
+# EMAIL_PORT = 465
+# EMAIL_HOST_USER = 'test@lygiai.org'
+# EMAIL_HOST_PASSWORD = 'bandymasbANdymas'
+# EMAIL_USE_SSL = True
 
