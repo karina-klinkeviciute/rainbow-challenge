23f4e5715f6a84f656bf6dcf8bdfa03c4b3873b8
karina <karina.klinkeviciute@gmail.com>
2021-08-25 14:38:33 +0300

    image to prize

--- rainbow/results/models/prize.py
+++ rainbow/results/models/prize.py
@@ -19,6 +19,7 @@ class Prize(models.Model):
     )
     price = models.IntegerField(verbose_name=_('price'))
     amount = models.IntegerField(verbose_name=_('amount'))
+    image = models.ImageField(verbose_name=_('image'), upload_to='prizes')
     available = models.BooleanField(verbose_name=_('available'), default=True)
 
     def __str__(self):
