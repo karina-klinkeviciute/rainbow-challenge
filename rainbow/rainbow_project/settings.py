1098c92ac8323972bc05bbdcab3db6d254fa13ee
Karina <karina.klinkeviciute@gmail.com>
2021-11-17 16:46:15 +0200

    changed email settings

--- rainbow/rainbow_project/settings.py
+++ rainbow/rainbow_project/settings.py
@@ -197,7 +197,7 @@ OTP_TOTP_ISSUER = 'Rainbow challenge'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'test@lygiai.org'
-EMAIL_HOST_PASSWORD = 'bandymasbANdymas'
 EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
