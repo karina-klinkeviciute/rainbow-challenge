a21619482800fae2832aea6bdb02a65369d98252
Willow Powell <willow@sojoodi.com>
2021-12-04 17:48:10 -0500

    Add call to load_dotenv to manage.py

--- rainbow/manage.py
+++ rainbow/manage.py
@@ -3,9 +3,12 @@
 import os
 import sys
 
+import dotenv
+
 
 def main():
     """Run administrative tasks."""
+    dotenv.load_dotenv()
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rainbow_project.settings')
     try:
         from django.core.management import execute_from_command_line
