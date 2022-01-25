080660b83c082fff5248e08c154682f072ad8b3d
Karina <karina.klinkeviciute@gmail.com>
2022-01-25 15:51:57 +0200

    time_sent read only in admin

--- rainbow/message/admin.py
+++ rainbow/message/admin.py
@@ -7,6 +7,7 @@ class MessageAdmin(admin.ModelAdmin):
     fields = ('user', "message_text", "admin_sender", "type", 'automated', 'time_sent', 'seen')
     list_display = ('user', "automated", "type", 'time_sent')
     list_filter = ('automated', "type", )
+    readonly_fields = ('time_sent', )
 
 
 admin.site.register(Message, MessageAdmin)
