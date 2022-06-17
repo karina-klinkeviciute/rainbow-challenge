f01a3ace9ad67b885b64ffe22b31b269d458c59a
Karina <karina.klinkeviciute@gmail.com>
2022-06-17 19:53:50 +0300

    fixed error in claimed prize - user is now being saved properly

--- rainbow/results/serializers/prize.py
+++ rainbow/results/serializers/prize.py
@@ -18,6 +18,29 @@ class ClaimedPrizeSerializer(serializers.ModelSerializer):
         fields = ['uuid', 'amount', 'prize', 'issued']
         read_only_fields = ['issued', ]
 
+    def create(self, validated_data):
+
+        # prize = validated_data.pop('main_joined_challenge')
+
+        # Check if this user can join this challenge.
+        # User can't join challenge if they have already done it and the challenge isn't 'multiple'
+        # challenge = main_joined_challenge_data['challenge']
+        user = self.context['request'].user
+        validated_data["user"] = user
+        return super().create(validated_data)
+        # main_joined_challenge_data["user"] = user
+        # if challenge.multiple is False:
+        #     if JoinedChallenge.objects.filter(challenge=challenge, user=user).exists():
+        #         raise serializers.ValidationError(_("This challenge can be joined only once."))
+        #
+        # this_challenge = self.Meta.model.objects.create(**validated_data)
+        #
+        # main_joined_challenge = JoinedChallenge.objects.create(**main_joined_challenge_data)
+        # main_joined_challenge.save()
+        # this_challenge.main_joined_challenge = main_joined_challenge
+        # this_challenge.save()
+        # return this_challenge
+
     def validate(self, data):
         data = super().validate(data)
         amount = data["amount"]
