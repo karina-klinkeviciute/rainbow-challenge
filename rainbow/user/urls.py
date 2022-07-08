5d2b9e3a4d93f0218981ee9a62244b24e74e65e8
Karina <karina.klinkeviciute@gmail.com>
2022-07-08 11:59:58 +0300

    completed challenges was pointing to joined challenges

--- rainbow/user/urls.py
+++ rainbow/user/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from joined_challenge.views.joined_challenge import UserJoinedChallengesAPIView
+from joined_challenge.views.joined_challenge import UserJoinedChallengesAPIView, UserCompletedChallengesAPIView
 from user import views
 
 urlpatterns = [
     path('gender/', views.GenderListView.as_view(), name='gender-list'),
     path('joined_challenges', UserJoinedChallengesAPIView.as_view(), name='user-joined-challenges'),
-    path('completed_challenges', UserJoinedChallengesAPIView.as_view(), name='user-completed-challenges'),
+    path('completed_challenges', UserCompletedChallengesAPIView.as_view(), name='user-completed-challenges'),
 
 ]
