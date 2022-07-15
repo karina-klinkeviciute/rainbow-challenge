c1c6db79f94e7b3011df2c56b3d050fcdbb14235
Karina <karina.klinkeviciute@gmail.com>
2022-07-15 19:13:07 +0300

    restored settings

--- rainbow/rainbow_project/settings.py
+++ rainbow/rainbow_project/settings.py
@@ -140,9 +140,9 @@ AUTH_PASSWORD_VALIDATORS = [
     # {
     #     'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     # },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    # },
 ]
 
 
