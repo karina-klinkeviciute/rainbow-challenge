265832087ea7bae32e72929dc02f6399a8c1440a
Karina <karina.klinkeviciute@gmail.com>
2022-09-07 17:37:07 +0300

    added uuid to text

--- rainbow/texts/models.py
+++ rainbow/texts/models.py
@@ -1,8 +1,14 @@
+import uuid
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
 class Text(models.Model):
+    uuid = models.UUIDField(
+        default=uuid.uuid4,
+        editable=False,
+        primary_key=True,
+    )
     body = models.TextField(verbose_name=_("body"))
     title = models.CharField(verbose_name=_("title"), max_length=255)
     notes = models.TextField(verbose_name=_("notes"))
