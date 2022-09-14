3165d59e66db26d30b2ddc8dc3991fe630ddb804
Karina <karina.klinkeviciute@gmail.com>
2022-09-14 22:02:58 +0300

    fixed circular import

--- rainbow/results/utils.py
+++ rainbow/results/utils.py
@@ -1,12 +1,11 @@
 from django.core.mail import send_mail
 
-from user.models import User
-
 
 def message_site_admins(subject: str, message: str):
     """
     Messages users that have status as admin.
     """
+    from user.models import User
     admins = User.objects.filter(is_admin=True).values_list("email", flat=True)
     send_mail(subject,
               message,
