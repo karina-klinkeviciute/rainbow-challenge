deceeb7bb261b2745470e95472b31ce41193df27
Karina <karina.klinkeviciute@gmail.com>
2022-12-01 20:24:57 +0200

    to the challenge confirmation screen added files.

--- rainbow/joined_challenge/views/confirmation.py
+++ rainbow/joined_challenge/views/confirmation.py
@@ -24,5 +24,4 @@ class ConfirmDetailView(LoginRequiredMixin, DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
-
         return context
