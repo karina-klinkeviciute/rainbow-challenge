692c3431d03382a4b24d41cf55ecc8b05c5810cb
Karina <karina.klinkeviciute@gmail.com>
2023-02-06 18:45:13 +0200

    djoser social urls

--- rainbow/rainbow_project/urls.py
+++ rainbow/rainbow_project/urls.py
@@ -70,6 +70,7 @@ urlpatterns = [
     # path('admin/', admin.site.urls),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
+    path('auth/', include('djoser.social.urls')),
     path('api/user/', include('user.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
