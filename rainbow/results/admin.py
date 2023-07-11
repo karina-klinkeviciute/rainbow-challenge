4ae0251cea1f292ad03767f5a6039d14be35f0ff
karina <karina.klinkeviciute@.>
2023-07-11 19:28:29 +0300

    gettext

--- rainbow/results/admin.py
+++ rainbow/results/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.utils.translation import gettext_lazy as _
+from django.utils.translation import gettext as _
 from fcm_django.models import FCMDevice
 
 # Register your models here.
