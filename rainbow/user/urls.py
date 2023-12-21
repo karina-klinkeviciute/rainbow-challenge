89a2ddcaf4fb68d6808e6fc4214a8c3c5441f5f2
karina <karina.klinkeviciute@.>
2023-12-21 16:49:01 +0200

    oauth token returned after google authentication

--- rainbow/user/urls.py
+++ rainbow/user/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 
 from joined_challenge.views.joined_challenge import UserJoinedChallengesAPIView, UserCompletedChallengesAPIView
 from user import views
-from user.views import OAuthStateCodeToken
+from user.views import OAuthStateCodeToken, OAuthTokenID
 
 urlpatterns = [
     path('gender/', views.GenderListView.as_view(), name='gender-list'),
     path('joined_challenges', UserJoinedChallengesAPIView.as_view(), name='user-joined-challenges'),
     path('completed_challenges', UserCompletedChallengesAPIView.as_view(), name='user-completed-challenges'),
     path('oauth_token/', OAuthStateCodeToken.as_view(), name='oauth-token-get'),
+    path('oauth_token_id', OAuthTokenID.as_view(), name='oauth-token-id-post')
+
 ]
--- rainbow/user/views.py
+++ rainbow/user/views.py
@@ -2,13 +2,20 @@ import json
 
 import logging
 import requests
+
 from django.views.generic import TemplateView
 from django.utils.translation import gettext_lazy as _
+from django.conf import settings
+
 from rest_framework import views, status
+from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework.permissions import BasePermission, SAFE_METHODS, AllowAny
 
-from user.models import GenderOptions
+from google.oauth2 import id_token
+from google.auth.transport import requests as google_requests
+
+from user.models import GenderOptions, User
 from user.serializers import GenderSerializer
 
 logger = logging.getLogger('root')
@@ -79,6 +86,35 @@ class OAuthStateCodeToken(views.APIView):
             response_status = status.HTTP_400_BAD_REQUEST
 
         return Response(content, status=response_status)
+
+class OAuthTokenID(views.APIView):
+    http_method_names = ('post', 'head', 'options')
+    permission_classes = [AllowAny, ]
+
+    def post(self, request, *args, **kwargs):
+        token = request.POST.get("token")
+        try:
+            # id_info = id_token.verify_oauth2_token(token, google_requests.Request())
+            # userid = id_info['sub']
+
+            response = requests.get("https://oauth2.googleapis.com/tokeninfo?id_token=eyJhbGciOiJSUzI1NiIsImtpZCI6IjBhZDFmZWM3ODUwNGY0NDdiYWU2NWJjZjVhZmFlZGI2NWVlYzllODEiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiI3MDQ5MzY0OTEzNDItZmZyamg1ajNkdDJraGpiZjE0ZmV1aG8wNjgzaDdndWkuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJhdWQiOiI3MDQ5MzY0OTEzNDItcDRmNWZhMGI2ZjJmdTBvaWl0Z2Fqbzg3aXExZHBucTkuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJzdWIiOiIxMDAyMTUxNDE0NzEzNDE5MzU2NDUiLCJlbWFpbCI6ImFwYWJpdGFzZGV2MUBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiYXRfaGFzaCI6Im9QVXRfUUtKV0lEbmhpZzZ5X0t0cnciLCJub25jZSI6Ik1nZTRqNDA3cDV5WXEtd2swdlkyaFlvQjZ1ZTN5OGdoSWxTUkJJdS1kSE0iLCJuYW1lIjoiQXBhYml0YXMgQXBhYml0YXMiLCJwaWN0dXJlIjoiaHR0cHM6Ly9saDMuZ29vZ2xldXNlcmNvbnRlbnQuY29tL2EvQUNnOG9jTGVjNWxYRVVoaHkzZ01GU3RETUVWcHFoei1NYl9XYThHelVkcTlQQTlZPXM5Ni1jIiwiZ2l2ZW5fbmFtZSI6IkFwYWJpdGFzIiwiZmFtaWx5X25hbWUiOiJBcGFiaXRhcyIsImxvY2FsZSI6ImVuLUdCIiwiaWF0IjoxNzAyNDEwNTYxLCJleHAiOjE3MDI0MTQxNjF9.qfKWHTZ7i8qw2naYNP3k4K6bWC4gxm9OCL9EG7tQiyGN9BpiCv1WP2zLbyWDZ--HAMOV1JM1--4O7RLs4oByoGS4-MvtzsPBJblI2JWvIQ1avdRcWclA7slZ9XSlsv68zHCIftNA8LT84V7SvvHLhVI7mjHO1Dp2av4D4UdbK9TcaNDz2yDFWVWC0z5QKfMsPsNWdlnktYCe07DPgNsyKM67vApT6Rf6xLIJqwQnYGyGlO_palG6ZBlnuHCVD6BEy-CKYctXCoPWIvdcLFNh6hOKJ2G_rapwgTajvHYDojI7ZVniYngDD6tsFlTwu0kP9TjhJyHacWpU4PDxClKIeQ")
+
+            print("RESPONSE:", response.text)
+
+            userid = response.json()["email"]
+
+            # userid = "karina.klinkeviciute@gmail.com"
+
+            # user_token = settings.TOKEN_MODEL.objects.get()
+            user = User.objects.get(email=userid)
+            user_token = Token.objects.get(user=user).key
+            print("USER EMAIL: ", userid)
+            print("USER TOKEN: ", user_token)
+
+            return Response(data={"auth_token": user_token})
+
+        except ValueError:
+            pass
 
 
 class PasswordResetView(TemplateView):
