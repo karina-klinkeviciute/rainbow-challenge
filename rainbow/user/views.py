a3c9b17cf2d07b33a587934f12701a52f68368ea
karina <karina.klinkeviciute@.>
2024-01-22 17:37:57 +0200

    apple id login veikia

--- rainbow/user/views.py
+++ rainbow/user/views.py
@@ -3,7 +3,10 @@ import json
 import logging
 import uuid
 
+import httpx
 import requests
+
+import jwt
 
 from django.views.generic import TemplateView
 from django.utils.translation import gettext_lazy as _
@@ -90,21 +93,76 @@ class OAuthStateCodeToken(views.APIView):
 
         return Response(content, status=response_status)
 
+
+def decode_access_token(authorisation_token):
+    # get public key from jwks uri
+    response = httpx.get(url="https://appleid.apple.com/auth/keys")
+
+    # gives the set of jwks keys.the keys has to be passed as it is to jwt.decode() for signature verification.
+    key = response.json()
+    print(key)
+
+    # get the algorithm type from the request header
+    algorithm = jwt.get_unverified_header(authorisation_token).get('alg')
+
+    user_info = jwt.decode(authorisation_token, key, algorithms=algorithm)
+
+    return user_info
+
+# šitas, atrodo, veikia
+def decode_and_validate_token(token):
+    unvalidated = jwt.decode(token, options={"verify_signature": False})
+    jwks_url = "https://appleid.apple.com/auth/keys"
+    jwks_client = jwt.PyJWKClient(jwks_url)
+    header = jwt.get_unverified_header(token)
+    key = jwks_client.get_signing_key(header["kid"]).key
+    return jwt.decode(token, key, [header["alg"]])
+
+# Another option:
+def decode_jwt_token(token: str, auth_domain: str, audience: str):
+    jwks_url = f'https://{auth_domain}/.well-known/jwks.json'
+    jwks_client = jwt.PyJWKClient(jwks_url)
+    header = jwt.get_unverified_header(token)
+    key = jwks_client.get_signing_key(header["kid"]).key
+    decoded = jwt.decode(token, key, [header["alg"]], audience=audience)
+    return decoded
+
+
+
 class OAuthTokenID(views.APIView):
     http_method_names = ('post', 'head', 'options')
     permission_classes = [AllowAny, ]
 
     def post(self, request, *args, **kwargs):
         token = request.POST.get("token")
-        try:
-            # id_info = id_token.verify_oauth2_token(token, google_requests.Request())
-            # userid = id_info['sub']
+        type = request.POST.get("type")
+        if type is None:
+            type = "google"
+
+        #     for testing only
+        # type = "apple"
+        # token = """eyJraWQiOiJsVkhkT3g4bHRSIiwiYWxnIjoiUlMyNTYifQ.eyJpc3MiOiJodHRwczovL2FwcGxlaWQuYXBwbGUuY29tIiwiYXVkIjoib3JnLnJhaW5ib3djaGFsbGVuZ2UiLCJleHAiOjE3MDU2Njk5MDUsImlhdCI6MTcwNTU4MzUwNSwic3ViIjoiMDAxNjQ3LjY0YTNlNWNmM2Y1OTQ2YTViMjdlNmFjZWQwNDU3MzVkLjE1MjciLCJjX2hhc2giOiJDQ2RITGpBQW1iOEdXamlFU2VOdkRnIiwiZW1haWwiOiJhcGFiaXRhc2RldjFAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOiJ0cnVlIiwiYXV0aF90aW1lIjoxNzA1NTgzNTA1LCJub25jZV9zdXBwb3J0ZWQiOnRydWV9.XI5_20oHzg-az708bJeBZOeRd3eozG96IYk2Q0HWqei1GCYGm_KDYThpRKg7T_kwXOEOjRJ_Blx7gjLmKhgWSVpBqOVaSFreoCiVNFRUBkbiZBoYeCJpSICh1ZOqq2_HpPZJTQN3R9xWvXYMAEls7zP_6lGSqLpC2FndijI7V_DAYB3hVFFIpRp_D9nnWOx2B6GRxD2jTato7qZ1mv2zEA0j5xRH5u0OcKxbb5hs9zxev1EiT9GYGWqX-pKopHvgtzOvSdAglygN9oAyoMpbQD-TEbu1-XzfwmdNcwi4QjMD4YGT5yfhRJr1My5j7WYm1Y4VVOSnSNkDtiaiLBKzWA"""
+
+        if type == "google":
 
             response = requests.get(f"https://oauth2.googleapis.com/tokeninfo?id_token={token}")
 
             print("RESPONSE:", response.text)
 
             userid = response.json()["email"]
+
+        if type == "apple":
+
+            user_data = decode_and_validate_token(token)
+
+            print("RESPONSE APPLE: ", user_data)
+
+            userid = user_data["email"]
+
+        try:
+            # id_info = id_token.verify_oauth2_token(token, google_requests.Request())
+            # userid = id_info['sub']
+
 
             # userid = "karina.klinkeviciute@gmail.com"
 
